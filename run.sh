206a0d877854701570fe2f934b7648984ea601bb
citergr2 <rustorusto26@gmail.com>
2025-06-29 10:12:58 +0700

    XitersToolsEnc.arm XitersToolsEnc.arm64 XitersToolsEnc.x86

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ganti 'script.enc' dengan nama file terenkripsi kamu
-ENC_FILE="XitersToolsEnc.enc"
-
-# Jalankan isi hasil dekripsi di RAM (tidak tersimpan di disk)
-openssl enc -aes-256-cbc -d -in "$ENC_FILE" | bash
